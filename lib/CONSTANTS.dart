9f9e732abd382a38efc62e6101e9745aa38e6f83
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-09-19 07:32:21 +0800

    :lipstick: Icon assets

---
+++ lib/CONSTANTS.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const primaryColour = Color(0xFFFF6C3B);
+const accentColour = Color(0xFF462990);
+
+const profileName = 'Alan Swimmer';
+const powerUsageText = '20.3';
