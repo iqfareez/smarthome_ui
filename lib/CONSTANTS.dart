8c891fadab29a1b0cb5145abdce1b9a093bf293c
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-09-19 09:37:45 +0800

    andi berandee

--- lib/CONSTANTS.dart
+++ lib/CONSTANTS.dart
@@ -5,5 +5,10 @@ import 'package:flutter/material.dart';
 const primaryColour = Color(0xFFFF6C3B);
 const accentColour = Color(0xFF462990);
 
+const paddingValue = 16.0;
+
 const profileName = 'Alan Swimmer';
 const powerUsageText = '20.3';
+const songTitle = 'Penat';
+const songArtist = 'Andi Bernadee';
+const songIconSize = 18.0;
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,11 +30,8 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Expanded(
               flex: 1,
-              child: Container(
-                color: accentColour,
-                child: Text('Music Playinh'),
-              ),
-            )
+              child: MusicPlayer(),
+            ),
           ],
         ),
       ),
@@ -47,45 +44,58 @@ class ProfileHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(paddingValue),
       child: Center(
         child: ListTile(
-            title: Text(
-              'Welcome home',
-              style: TextStyle(color: Colors.grey),
-            ),
-            subtitle: Text(
-              profileName,
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 31),
-            ),
-            trailing: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.asset(
-                'images/profileImage.jpg',
-                fit: BoxFit.fill,
-              ),
-            )),
+          title: Text(
+            'Welcome home',
+            style: TextStyle(color: Colors.grey),
+          ),
+          subtitle: Text(
+            profileName,
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 31),
+          ),
+          trailing: ProfileImage(
+            imageLocation: 'images/profileImage.jpg',
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileImage extends StatelessWidget {
+  const ProfileImage({Key key, this.imageLocation}) : super(key: key);
+
+  final String imageLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8.0),
+      child: Image.asset(
+        imageLocation,
+        fit: BoxFit.fill,
       ),
     );
   }
 }
 
 class PowerUsageBanner extends StatelessWidget {
+  final double baseRadius = 28.0;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(paddingValue),
       child: ListTile(
         leading: CircleAvatar(
           //this CircleAvatar is for border
-          radius: 25,
+          radius: baseRadius,
           backgroundColor: Colors.grey.shade300,
           child: CircleAvatar(
             //This CircleAvatar is for icon
-            radius: 24,
+            radius: baseRadius - 1,
             backgroundColor:
                 Colors.grey[50], //same as scaffold background colour,
             child: FaIcon(
@@ -111,6 +121,79 @@ class PowerUsageBanner extends StatelessWidget {
           'Power usage for today',
           style: TextStyle(color: Colors.grey),
         ),
+      ),
+    );
+  }
+}
+
+class MusicPlayer extends StatefulWidget {
+  @override
+  _MusicPlayerState createState() => _MusicPlayerState();
+}
+
+class _MusicPlayerState extends State<MusicPlayer> {
+  List<FaIcon> favIcon = [
+    FaIcon(FontAwesomeIcons.solidHeart,
+        size: songIconSize, color: Colors.white),
+    FaIcon(FontAwesomeIcons.heart, size: songIconSize, color: Colors.white),
+  ];
+  List<Icon> pauseIcon = [
+    Icon(Icons.pause,
+        color: Colors.white, size: songIconSize),
+    Icon(Icons.play_arrow, color: Colors.white, size: songIconSize)
+  ];
+
+  bool isPaused = true;
+  bool isFav = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: accentColour,
+      child: Padding(
+        padding: const EdgeInsets.all(paddingValue),
+        child: ListTile(
+            title: Text(
+              songTitle,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+              songArtist,
+              style: TextStyle(color: Colors.purple.shade300),
+            ),
+            leading: ProfileImage(
+              imageLocation: 'images/albumArt.jpg',
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                    icon: isFav ? favIcon[0] : favIcon[1],
+                    onPressed: () {
+                      setState(() {
+                        isFav = !isFav;
+                      });
+                    }),
+                SizedBox(width: 5),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isPaused = !isPaused;
+                    });
+                  },
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: primaryColour,
+                    ),
+                    child: isPaused ? pauseIcon[0] : pauseIcon[1],
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
